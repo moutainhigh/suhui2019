7571d8c4498e1666f460f34cce72f9f968430f1c
1056640575@qq.com <wch1056640575>
2019-07-17 23:01:15 +0800

    绑定银行卡2

---
+++ suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/modules/bind/mapper/SysUserBindMapper.java
@@ -0,0 +1,17 @@
+package org.suhui.modules.bind.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.suhui.modules.bind.entity.SysUserBind;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 用户绑定关系
+ * @Author: jeecg-boot
+ * @Date:   2019-07-15
+ * @Version: V1.0
+ */
+public interface SysUserBindMapper extends BaseMapper<SysUserBind> {
+
+}
---
+++ suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/modules/bind/service/ISysUserBindService.java
@@ -0,0 +1,14 @@
+package org.suhui.modules.bind.service;
+
+import org.suhui.modules.bind.entity.SysUserBind;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 用户绑定关系
+ * @Author: jeecg-boot
+ * @Date:   2019-07-15
+ * @Version: V1.0
+ */
+public interface ISysUserBindService extends IService<SysUserBind> {
+
+}
