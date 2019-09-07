b5500bc75248b3064339368e17ac0e4925011310
wch <wangchunheme@163.com>
2019-09-07 21:28:35 +0800

    换汇冻结  汇率维护  用户账户类型维护

--- suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/modules/api/controller/AppLoginPayExchangeController.java
+++ suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/modules/api/controller/AppLoginPayExchangeController.java
@@ -81,7 +81,7 @@ public class AppLoginPayExchangeController {
 
                     // 设置 冻结金额
                     long frozen_amount_before =  Long.parseLong(mapAssetDb.get("frozen_amount")+"") ;// 冻结金额
-                    long available_amount = available_amount_before;
+                    long available_amount = available_amount_before-moneyamount_long;
                     long frozen_amount = frozen_amount_before+ moneyamount_long ;
                     PayAccountAsset payAccountAsset = new PayAccountAsset() ;
                     payAccountAsset.setId(Integer.parseInt(mapAssetDb.get("id")+"")) ;
@@ -110,6 +110,76 @@ public class AppLoginPayExchangeController {
     }
 
 
+    /**
+     *  换汇解冻
+     * @param params
+     * @return
+     */
+    @RequestMapping(value = "/unfrozen", method = RequestMethod.POST)
+    @Transactional
+    public Result<JSONObject> unfrozen(HttpServletRequest request, HttpServletResponse response, @RequestParam Map<String, Object> params ) {
+        //用户退出逻辑
+        Result<JSONObject> result = new Result<JSONObject>();
+        JSONObject obj = new JSONObject();
 
+        try{
+
+            String userno = params.get("userno")+"" ; //用户id
+            String usertype = params.get("usertype")+"" ; //用户类型 0-默认 1-个人 2-企业',
+            String account_type_code = params.get("account_type_code")+"" ; //账户类型
+            String moneyamount = params.get("moneyamount") +""; // 获取
+            long moneyamount_long = Long.parseLong(moneyamount) ;
+            Map map = new HashMap() ;
+            map.put("userno" , userno) ;
+            map.put("usertype" , usertype) ;
+            map.put("account_type_code" , account_type_code) ;
+            Map payaccount =  iPayAccountService.getPayAccountByUserNo(map) ;
+
+            String is_allow_transfer_out = payaccount.get("is_allow_transfer_out")+"" ;
+            if(is_allow_transfer_out.equals("0")){ // 是否允许转汇
+                // 操作账户金额
+                String account_no = payaccount.get("account_no") +"";
+                String identity_no = payaccount.get("identity_no")+"" ;
+                String identity_type = payaccount.get("identity_type")+"" ;
+                Map mapAsset = new HashMap() ;
+                mapAsset.put("account_no" , account_no) ;
+                mapAsset.put("account_type_code" , account_type_code) ;
+                Map<String,Object> mapAssetDb = iPayAccountService.getPayAccountAssetByUserNo(mapAsset) ;
+                long frozen_amount = Long.parseLong(mapAssetDb.get("frozen_amount")+"")  ; // 可用金额
+                if(moneyamount_long >frozen_amount ){
+                    result.error("frozen amount is not right");
+                    TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                    return result ;
+                }else{
+
+                    // 设置 冻结金额
+                    long frozen_amount_before =  Long.parseLong(mapAssetDb.get("frozen_amount")+"") ;// 解冻金额
+                    long available_amount = available_amount_before;
+                    long frozen_amount = frozen_amount_before- moneyamount_long ;
+                    PayAccountAsset payAccountAsset = new PayAccountAsset() ;
+                    payAccountAsset.setId(Integer.parseInt(mapAssetDb.get("id")+"")) ;
+                    payAccountAsset.setFrozenAmount(frozen_amount) ; // 冻结金额
+                    payAccountAsset.setAvailableAmount(available_amount) ; // 设置可用金额
+                    iPayAccountAssetService.updateById(payAccountAsset) ;
+                }
+
+            }else{
+                result.error("not allow to transfer out"); // 不允许 转出  同样就没有解冻了。
+                TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                return result ;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+            result.error("操作失败");
+            return result ;
+        }
+
+        result.setResult(obj);
+        result.success("换汇冻结");
+        result.setCode(CommonConstant.SC_OK_200);
+        return result ;
+    }
 
 }
