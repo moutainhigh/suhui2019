de5fad9b8baae6fe9c41afe984359a6c9c34d700
zengsiyuan <1056637734@qq.com>
2020-01-22 23:05:12 +0800

    修改接口

---
+++ suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/config/LogParamAspect.java
@@ -0,0 +1,100 @@
+package org.suhui.config;
+
+
+import com.alibaba.fastjson.JSON;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.suhui.modules.ngalain.aop.LogRecordAspect;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Configuration//定义一个切面
+public class LogParamAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogRecordAspect.class);
+
+    // 定义切点Pointcut
+    @Pointcut("execution(* org.suhui.modules.order.controller..*.*(..))")
+    public void excudeService() {
+    }
+
+
+
+
+    @Around("excudeService()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+
+        String url = request.getRequestURL().toString();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        String queryString = request.getQueryString();
+        Object[] args = pjp.getArgs();
+        String params = "";
+        //获取请求参数集合并进行遍历拼接
+        if(args.length>0){
+
+            if("POST".equals(method)){
+                Object object = args[0];
+                params = beanToString(args[1]);
+            }else if("GET".equals(method)){
+                params = queryString;
+            }
+        }
+        logger.info("请求开始===地址:"+url);
+        logger.info("请求开始===类型:"+method);
+        logger.info("请求开始===参数:"+params);
+        return "";
+    }
+
+    public static <T> String beanToString(T value) {
+        Class<?> clazz = value.getClass();
+        if(clazz == int.class || clazz == Integer.class) {
+            return value + "";
+        }else if(clazz == String.class) {
+            return (String)value;
+        }else if(clazz == long.class || clazz == Long.class) {
+            return value + "";
+        }else {
+            return JSON.toJSONString(value);
+        }
+    }
+
+    public static Map<String, Object> getKeyAndValue(Object obj) {
+        Map<String, Object> map = new HashMap<>();
+        // 得到类对象
+        Class userCla = (Class) obj.getClass();
+        /* 得到类中的所有属性集合 */
+        Field[] fs = userCla.getDeclaredFields();
+        for (int i = 0; i < fs.length; i++) {
+            Field f = fs[i];
+            f.setAccessible(true); // 设置些属性是可以访问的
+            Object val = new Object();
+            try {
+                val = f.get(obj);
+                // 得到此属性的值
+                map.put(f.getName(), val);// 设置键值
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return map;
+    }
+}
