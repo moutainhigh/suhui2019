1308b19292b28e4c4181bc2ec2f0474cb2a8a1e1
zengsiyuan <1056637734@qq.com>
2020-02-04 11:46:48 +0800

    修改接口

--- suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/modules/order/entity/OrderAssurer.java
+++ suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/modules/order/entity/OrderAssurer.java
@@ -2,6 +2,7 @@ package org.suhui.modules.order.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -13,221 +14,260 @@ import org.suhui.modules.utils.BaseUtil;
 /**
  * @Description: 去
  * @Author: jeecg-boot
- * @Date:   2019-12-29
+ * @Date: 2019-12-29
  * @Version: V1.0
  */
 @Data
 @TableName("order_assurer")
 public class OrderAssurer implements Serializable {
     private static final long serialVersionUID = 1L;
-    
-	/**id*/
-	@TableId(type = IdType.UUID)
-	private java.lang.String id;
-	/**用户编号*/
-	private java.lang.String userNo;
-	/**承兑商名称*/
-	private java.lang.String assurerName;
-	/**承兑商国家码*/
-	private java.lang.String countryCode;
-	/**承兑商状态(待审核(to_audit)、正常接单(normal)、余额不足(lack_balance)、禁止接单(ban_order))*/
-	private java.lang.String assurerState;
-	/**费率*/
-	private java.lang.Double assurerRate;
-	/**可用额度*/
-	private java.lang.Double canUseLimit;
-	/**已用额度*/
-	private java.lang.Double usedLimit;
-	/**总额度=已用额度+可用额度*/
-	private java.lang.Double totalLimit;
-	/**策略(单一制(one)、多单制(more))*/
-	private java.lang.String assurerStrategy;
-	/**创建人*/
-	private java.lang.String createBy;
-	/**创建时间*/
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-	private java.util.Date createTime;
-	/**更新人*/
-	private java.lang.String updateBy;
-	/**更新时间*/
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
-	private java.util.Date updateTime;
-	/**删除状态（0，正常，1已删除）*/
-	private java.lang.String delFlag;
-	private java.lang.String assurerPhone;
-	private java.lang.Integer onlineState;
-	private java.lang.Double payLockMoney;
-	public static long getSerialVersionUID() {
-		return serialVersionUID;
-	}
+
+    /**
+     * id
+     */
+    @TableId(type = IdType.UUID)
+    private java.lang.String id;
+    /**
+     * 用户编号
+     */
+    private java.lang.String userNo;
+    /**
+     * 承兑商名称
+     */
+    private java.lang.String assurerName;
+    /**
+     * 承兑商国家码
+     */
+    private java.lang.String countryCode;
+    /**
+     * 承兑商状态(待审核(to_audit)、正常接单(normal)、余额不足(lack_balance)、禁止接单(ban_order))
+     */
+    private java.lang.String assurerState;
+    /**
+     * 费率
+     */
+    private java.lang.Double assurerRate;
+    /**
+     * 可用额度
+     */
+    private java.lang.Double canUseLimit;
+    /**
+     * 已用额度
+     */
+    private java.lang.Double usedLimit;
+    /**
+     * 总额度=已用额度+可用额度
+     */
+    private java.lang.Double totalLimit;
+    /**
+     * 策略(单一制(one)、多单制(more))
+     */
+    private java.lang.String assurerStrategy;
+    /**
+     * 创建人
+     */
+    private java.lang.String createBy;
+    /**
+     * 创建时间
+     */
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private java.util.Date createTime;
+    /**
+     * 更新人
+     */
+    private java.lang.String updateBy;
+    /**
+     * 更新时间
+     */
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private java.util.Date updateTime;
+    /**
+     * 删除状态（0，正常，1已删除）
+     */
+    private java.lang.String delFlag;
+    private java.lang.String assurerPhone;
+    private java.lang.Integer onlineState;
+    private java.lang.Double payLockMoney;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
 
 
-	public void changeMoneyToPoints(){
-        if(BaseUtil.Base_HasValue(this.totalLimit)){
-            this.totalLimit = this.totalLimit*100;
+    public void changeMoneyToPoints() {
+        if (BaseUtil.Base_HasValue(this.totalLimit)) {
+            this.totalLimit = this.totalLimit * 100;
         }
-        if(BaseUtil.Base_HasValue(this.canUseLimit)){
-            this.canUseLimit = this.canUseLimit*100;
+        if (BaseUtil.Base_HasValue(this.canUseLimit)) {
+            this.canUseLimit = this.canUseLimit * 100;
         }
-        if(BaseUtil.Base_HasValue(this.usedLimit)){
-            this.usedLimit = this.usedLimit*100;
+        if (BaseUtil.Base_HasValue(this.usedLimit)) {
+            this.usedLimit = this.usedLimit * 100;
         }
-		if(BaseUtil.Base_HasValue(this.payLockMoney)){
-            this.payLockMoney = this.payLockMoney*100;
+        if (BaseUtil.Base_HasValue(this.payLockMoney)) {
+            this.payLockMoney = this.payLockMoney * 100;
         }
-	}
+    }
 
-	public void changeMoneyToBig(){
-		this.canUseLimit = this.canUseLimit/100;
-		this.usedLimit = this.usedLimit/100;
-		this.totalLimit = this.totalLimit/100;
-		this.payLockMoney = this.payLockMoney/100;
-	}
+    public void changeMoneyToBig() {
+        if (BaseUtil.Base_HasValue(this.canUseLimit) && this.canUseLimit > 0) {
+            this.canUseLimit = this.canUseLimit / 100;
+        }
+        if (BaseUtil.Base_HasValue(this.usedLimit) && this.usedLimit > 0) {
+            this.usedLimit = this.usedLimit / 100;
+        }
+        if (BaseUtil.Base_HasValue(this.totalLimit) && this.totalLimit > 0) {
+            this.totalLimit = this.totalLimit / 100;
+        }
+        if (BaseUtil.Base_HasValue(this.payLockMoney) && this.payLockMoney > 0) {
+            this.payLockMoney = this.payLockMoney / 100;
+        }
+    }
 
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getUserNo() {
-		return userNo;
-	}
+    public String getUserNo() {
+        return userNo;
+    }
 
-	public void setUserNo(String userNo) {
-		this.userNo = userNo;
-	}
+    public void setUserNo(String userNo) {
+        this.userNo = userNo;
+    }
 
-	public String getAssurerName() {
-		return assurerName;
-	}
+    public String getAssurerName() {
+        return assurerName;
+    }
 
-	public void setAssurerName(String assurerName) {
-		this.assurerName = assurerName;
-	}
+    public void setAssurerName(String assurerName) {
+        this.assurerName = assurerName;
+    }
 
-	public String getCountryCode() {
-		return countryCode;
-	}
+    public String getCountryCode() {
+        return countryCode;
+    }
 
-	public void setCountryCode(String countryCode) {
-		this.countryCode = countryCode;
-	}
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
 
-	public String getAssurerState() {
-		return assurerState;
-	}
+    public String getAssurerState() {
+        return assurerState;
+    }
 
-	public void setAssurerState(String assurerState) {
-		this.assurerState = assurerState;
-	}
+    public void setAssurerState(String assurerState) {
+        this.assurerState = assurerState;
+    }
 
-	public Double getAssurerRate() {
-		return assurerRate;
-	}
+    public Double getAssurerRate() {
+        return assurerRate;
+    }
 
-	public void setAssurerRate(Double assurerRate) {
-		this.assurerRate = assurerRate;
-	}
+    public void setAssurerRate(Double assurerRate) {
+        this.assurerRate = assurerRate;
+    }
 
-	public Double getCanUseLimit() {
-		return canUseLimit;
-	}
+    public Double getCanUseLimit() {
+        return canUseLimit;
+    }
 
-	public void setCanUseLimit(Double canUseLimit) {
-		this.canUseLimit = canUseLimit;
-	}
+    public void setCanUseLimit(Double canUseLimit) {
+        this.canUseLimit = canUseLimit;
+    }
 
-	public Double getUsedLimit() {
-		return usedLimit;
-	}
+    public Double getUsedLimit() {
+        return usedLimit;
+    }
 
-	public void setUsedLimit(Double usedLimit) {
-		this.usedLimit = usedLimit;
-	}
+    public void setUsedLimit(Double usedLimit) {
+        this.usedLimit = usedLimit;
+    }
 
-	public Double getTotalLimit() {
-		return totalLimit;
-	}
+    public Double getTotalLimit() {
+        return totalLimit;
+    }
 
-	public void setTotalLimit(Double totalLimit) {
-		this.totalLimit = totalLimit;
-	}
+    public void setTotalLimit(Double totalLimit) {
+        this.totalLimit = totalLimit;
+    }
 
-	public String getAssurerStrategy() {
-		return assurerStrategy;
-	}
+    public String getAssurerStrategy() {
+        return assurerStrategy;
+    }
 
-	public void setAssurerStrategy(String assurerStrategy) {
-		this.assurerStrategy = assurerStrategy;
-	}
+    public void setAssurerStrategy(String assurerStrategy) {
+        this.assurerStrategy = assurerStrategy;
+    }
 
-	public String getCreateBy() {
-		return createBy;
-	}
+    public String getCreateBy() {
+        return createBy;
+    }
 
-	public void setCreateBy(String createBy) {
-		this.createBy = createBy;
-	}
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
 
-	public Date getCreateTime() {
-		return createTime;
-	}
+    public Date getCreateTime() {
+        return createTime;
+    }
 
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
 
-	public String getUpdateBy() {
-		return updateBy;
-	}
+    public String getUpdateBy() {
+        return updateBy;
+    }
 
-	public void setUpdateBy(String updateBy) {
-		this.updateBy = updateBy;
-	}
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
 
-	public Date getUpdateTime() {
-		return updateTime;
-	}
+    public Date getUpdateTime() {
+        return updateTime;
+    }
 
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 
-	public String getDelFlag() {
-		return delFlag;
-	}
+    public String getDelFlag() {
+        return delFlag;
+    }
 
-	public void setDelFlag(String delFlag) {
-		this.delFlag = delFlag;
-	}
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag;
+    }
 
-	public Integer getOnlineState() {
-		return onlineState;
-	}
+    public Integer getOnlineState() {
+        return onlineState;
+    }
 
-	public void setOnlineState(Integer onlineState) {
-		this.onlineState = onlineState;
-	}
+    public void setOnlineState(Integer onlineState) {
+        this.onlineState = onlineState;
+    }
 
-	public Double getPayLockMoney() {
-		return payLockMoney;
-	}
+    public Double getPayLockMoney() {
+        return payLockMoney;
+    }
 
-	public void setPayLockMoney(Double payLockMoney) {
-		this.payLockMoney = payLockMoney;
-	}
+    public void setPayLockMoney(Double payLockMoney) {
+        this.payLockMoney = payLockMoney;
+    }
 
-	public String getAssurerPhone() {
-		return assurerPhone;
-	}
+    public String getAssurerPhone() {
+        return assurerPhone;
+    }
 
-	public void setAssurerPhone(String assurerPhone) {
-		this.assurerPhone = assurerPhone;
-	}
+    public void setAssurerPhone(String assurerPhone) {
+        this.assurerPhone = assurerPhone;
+    }
 }
