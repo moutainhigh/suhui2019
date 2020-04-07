f6913ee2e3d1d82324c49733d5f52c5b314c1f2a
蔡珊珊 <18305810165@163.com>
2020-04-07 23:41:52 +0800

    补充提交

--- suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/modules/toB/controller/MerchantController.java
+++ suhui-boot/suhui-boot-module-system/src/main/java/org/suhui/modules/toB/controller/MerchantController.java
@@ -1,10 +1,10 @@
 package org.suhui.modules.toB.controller;
 
 /**
- * @program: suhui-boot
- * @description: 商户业务
- * @author: 作者名称
- * @date: 2020-04-07 22:55
+ * 类说明：商户业务
+ *
+ * @author: 蔡珊珊
+ * @create: 2020-04-07 22:55
  **/
 public class MerchantController {
 }
